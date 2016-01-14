dbd91cbba3606666f62399b971ffb85205963c1b
Anton Stepan <info@antonstepan.it>
2016-01-14 08:59:42 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
+var $header = $('header').height();
+var $navigation = $('nav');
+var navSticky = 'nav-sticky';
 
 $(window).scroll(function() {
-  var header = $('header').height();
-  var navigation = $('nav');
-  var navSticky = 'nav-sticky';
-
-  if ($(this).scrollTop() > header) {
+  var thresholdReached = $(this).scrollTop() > header;
+  if (thresholdReached && !navigation.hasClass(navSticky)) {
     navigation.addClass(navSticky).fadeIn();
-  } else {
+  } else if (!thresholdReached && navigation.hasClass(navSticky)) {
     navigation.removeClass(navSticky);
   }
 });
