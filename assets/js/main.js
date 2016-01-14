1334a090617e8410d9b300ecf1d8ed6047d7451d
Anton Stepan <info@antonstepan.it>
2016-01-14 09:04:47 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 var $header = $('header').height();
 var $navigation = $('nav');
 var navSticky = 'nav-sticky';
+var cssNames = 'nav-sticky transition';
 
 $(window).scroll(function() {
   var thresholdReached = $(this).scrollTop() > header;
   if (thresholdReached && !navigation.hasClass(navSticky)) {
-    navigation.addClass(navSticky transition).fadeIn();
+    navigation.addClass(cssNames).fadeIn();
   } else if (!thresholdReached && navigation.hasClass(navSticky)) {
-    navigation.removeClass(navSticky transition);
+    navigation.removeClass(cssNames);
   }
 });
